f04982f463d5f0db6afea2d50152eaa10ae07244
Zql <1109873997@qq.com>
2019-07-29 08:19:13 +0800

    test33333543545

---
+++ GitHubTest02/src/main/java/Test01.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Test01 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println("我是" + random.nextInt());
+    }
+}
