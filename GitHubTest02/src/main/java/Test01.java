345fcf426ae213b7f09bf7128ad6ca3c29377f7e
Zql <1109873997@qq.com>
2019-07-29 08:19:40 +0800

    test33333543545

--- GitHubTest02/src/main/java/Test01.java
+++ GitHubTest02/src/main/java/Test01.java
@@ -4,5 +4,9 @@ public class Test01 {
     public static void main(String[] args) {
         Random random = new Random();
         System.out.println("我是" + random.nextInt());
+        System.out.println("我是" + random.nextInt());
+        System.out.println("我是" + random.nextInt());
+        System.out.println("我是" + random.nextInt());
+        System.out.println("我是" + random.nextInt());
     }
 }
